7b7159af2e0b1eb97bc2413898d2bc00be870810
sproloy <proloy99@gmail.com>
2017-05-30 12:51:50 +0530

    changes sql

---
+++ Artifacts/Sql/sql.ps1
@@ -0,0 +1,40 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+$url = "https://go.microsoft.com/fwlink/?LinkID=799012"
+$codeSetupUrl = Get-RedirectedUrl -URL $url
+
+$infPath = $PSScriptRoot + "\sql.inf"
+$sqlcodeSetup = "${env:Temp}\SQLServer2016-SSEI-Expr.exe"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $sqlcodeSetup)
+}
+catch
+{
+    Write-Error "Failed to download sql setup"
+}
+
+try
+{
+    Start-Process -FilePath $sqlcodeSetup -ArgumentList "/VERYSILENT /MERGETASKS=!runcode /LOADINF=$infPath"
+}
+catch
+{
+    Write-Error 'Failed to install sql server'
+}
